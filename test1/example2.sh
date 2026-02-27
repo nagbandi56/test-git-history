18d7b5aee7dc3ea20ea7b5b9c47c5c1f583d1a06
Nag bandi <nagbandi@hotmail.com>
2026-02-27 10:46:42 +0530

    updated

--- test1/example2.sh
+++ test1/example2.sh
@@ -2,3 +2,17 @@ This is second file
 
 accesskey = "KEYVAULT"
 secretkey = "CHANDU_KEY"
+
+
+this slkdfh a
+sdkjhf kashdf 
+alsdf lkasjd f
+asd lfasdf
+k 
+
+
+if (condition) {
+  sdjf a
+  sjkdhf a;sdf
+  asdfsdf
+}
